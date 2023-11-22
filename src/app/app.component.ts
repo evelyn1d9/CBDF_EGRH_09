f5116cbeea8a1c08da0a7eef9e150cca681650ec
evelyn1d9 <evelyn_1d9@hotmail.com>
2023-11-22 08:05:32 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,23 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'CBDF_EGRH_9.1';
+  nombre = "Evelyn Rocha";
+  edad = 21;
+  semestre = "Quinto semestre";
+  calificaciones = [8,7,10,10,8,9,8];
+
+
+  calcularPromedio(){
+   let suma = 0;
+   const longitudArreglo = this.calificaciones.length;
+   for (let i = 0; i<this.calificaciones.length; i++)
+   suma+=this.calificaciones[i];
+  return suma/longitudArreglo; 
+  }
+  estadoAlumno(){
+    if(this.calcularPromedio()>=6)
+    return "Aprobado";
+  else 
+  return "No aprobado";
+  }
 }
